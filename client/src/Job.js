23d97b763115887a08e640fb502443bd26c62dcd
Page Nathan (DC-MH/ENG1.13-NA) <nathan.page@bosch.com>
2020-06-25 13:30:25 -0400

    Added Job.js to fetch new job

---
+++ client/src/Job.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import Typography from '@material-ui/core/Typography'
+
+export default function Jobs()
+{
+    return(
+        <div> 
+            <Typography variant="h1">
+                Job Title
+            </Typography>
+        </div>
+    )
+}
