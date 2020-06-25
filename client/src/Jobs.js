c8ca09cbf8808fdbd2148cf7a8fb82ebf05c441d
Page Nathan (DC-MH/ENG1.13-NA) <nathan.page@bosch.com>
2020-06-25 13:30:57 -0400

    Added Jobs.js to collect new jobs from various sournces

---
+++ client/src/Jobs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Typography from '@material-ui/core/Typography'
+import Job from './Job.js'
+
+export default function Jobs()
+{
+    return(
+        <div> 
+            <Typography variant="h1">
+                Entry Level Software Jobs
+            </Typography>
+        </div>
+    )
+}
