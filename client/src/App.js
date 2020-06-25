38e0dba5c023cac66f83ac90387439b4c666cca6
Page Nathan (DC-MH/ENG1.13-NA) <nathan.page@bosch.com>
2020-06-25 13:31:56 -0400

    Added Jobs component in App.js to display list of jobs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jobs from './Jobs.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Jobs />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +22,12 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        
+
+      </body>
+
+
     </div>
   );
 }
