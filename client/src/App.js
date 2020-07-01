65ecd869e28b3f45c536d1e3d7311277557acff2
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-01 16:59:01 -0400

    Added async function fetchJobs and call using useEffect withing App()

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,28 @@ const mockJobs = [
   {title: 'Software Engineer 1', company:'Amazon'}
 ];
 
+const JOB_API_URL = 'http://localhost:3001/jobs'; //define the job api url
+
+async function fetchJobs(updateCB){
+  const res = await fetch(JOB_API_URL); //fetch() is a built-in browser function
+  const json = await res.json();  // wait for the json response
+  // console.log({json});
+  updateCB(json);
+}
+
 function App() {
+const [jobList, updateJobs] = React.useState([]);
+React.useEffect(()=>{
+  fetchJobs(updateJobs);
+}, []);
+
   return (
     <div className="App">
       <header className="App-header">
         Entry Level Software Engineer Jobs
       </header>
 
-      <Jobs jobs={mockJobs} />
+      <Jobs jobs={jobList} />
     
     </div>
   );
