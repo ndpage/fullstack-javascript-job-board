db5f77b2981d5c9caba4b3f95cf30691d5b5b1f0
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-01 18:04:54 -0400

    Updated Job component

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
 
 export default function Job({job})
 {
     return(
-        <div className="job">
-            <div class="job-title">{job.title}</div> 
-            <div class="job-company">{job.company}</div> 
-        </div>
+        
+        <Paper className='job'>
+            <div>  
+                <Typography variant="h5">{job.title}  </Typography> 
+                <Typography variant="h6">{job.company}</Typography>
+                <Typography variant="h6">{job.location}</Typography>
+            </div>
+            <div>   
+                <Typography> {job.created_at.split(' ').slice(0,3).join(' ')}</Typography>
+            </div>
+        </Paper>
     )
 }
