0d568aab7169bbca1604e28010354825a9e5eab8
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-07-03 17:40:13 -0400

    Added code to check for job title before loading job. Also, added code to display the job description using npm module html-react-parse

--- client/src/JobModal.js
+++ client/src/JobModal.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -8,6 +7,51 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 
-export default function JobModal({job}){
 
-}
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+  
+  export default function JobMoal({job, open, handleClose}) {
+    if(!job.title){
+        return <div /> 
+    }
+    return (
+      <div>
+       
+        <Dialog
+          open={open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+              {job.title}-{job.company}
+              <img src={job.company_logo} className={'job-modal-logo'} alt="company logo"/>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+            <div>
+                {require('html-react-parser')(
+                    `${job.description}`
+                )}
+            </div>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Close
+            </Button>
+
+            <a href={job.url}> 
+                <Button onClick={handleClose} color="primary">
+                Apply
+                </Button>
+            </a>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
