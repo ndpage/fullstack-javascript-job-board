0a316fa713dc7195c421beec8a6c594d35f709b2
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-23 14:34:37 -0400

    Added comments for App() function

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+
 import React from 'react';
 import './App.css';
 import Jobs from './Jobs.js'
 
-const JOB_API_URL = 'http://localhost:3001/jobs'; //define the job api url
 
+const JOB_API_URL = 'http://localhost:3001/api/jobs'; //define the api URL
+
+/*
+ *  Function: fetchJobs(UpdateCB) 
+ *  Description: Gets the job list as a JSON response from the API (See API URL above)
+ */
 async function fetchJobs(updateCB){
   const res = await fetch(JOB_API_URL); //fetch() is a built-in browser function
   const json = await res.json();  // wait for the json response
@@ -22,7 +28,6 @@ React.useEffect(()=>{
       <header className="App-header">
         Entry Level Jobs
       </header>
-
       <Jobs className="jobs" jobs={jobList} />
     </div>
 
