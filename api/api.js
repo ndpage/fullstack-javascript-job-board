bbc874601ffc7cf9c70303f1ec7dd1eb6f4170a4
Nathan Page <33336363+ndpage@users.noreply.github.com>
2020-09-23 14:34:24 -0400

    Changed API base URL to /api/ and added route /api/test

--- api/api.js
+++ api/api.js
@@ -7,13 +7,17 @@ var client = redis.createClient();
 const { promisify } = require("util");
 const getAsync = promisify(client.get).bind(client);
 
-app.get('/jobs', async(req, res) => {
+app.get('/api/jobs', async(req, res) => {
     const jobs = await getAsync('GitHub');
     res.header("Access-Control-Allow-Origin", "http://localhost:3000")
     console.log(JSON.parse(jobs).length);
-
-    return res.send(jobs)
-    
+    return res.send(jobs) 
+})
+app.get('/api/test', async(req, res) => {
+  
+    return(
+        res.status(201).json({hello:'World'})
+    ) 
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
